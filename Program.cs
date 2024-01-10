da7f3a62d4d1fea20caae55b89aa43dcece54869
albertanepali <albertanepali@gmail.com>
2024-01-09 19:40:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        string str = "";
+        string reverse="";
+        int Length = 0;
+
+        Console.WriteLine("Enter the string here to reverse");
+        str = Console.ReadLine();
+        Length = str.Length - 1;
+        while (Length >=0)
+        {
+            reverse = reverse + str[Length];
+            Length--;
+        }
+
+        Console.WriteLine("Your reverse string is "+reverse);
+        Console.ReadLine();
+
+          
+
+
+    }
+}
